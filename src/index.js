06b2b91f7ebc3b21c8d137828a5661e418ada0aa
Diego Ramos <diego.coderamos@gmail.com>
2019-09-15 22:15:18 -0300

    update PostController

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 
@@ -8,5 +9,9 @@ mongoose.connect(
   { useNewUrlParser: true, useUnifiedTopology: true }
 );
 
+app.use(
+  '/files',
+  express.static(path.resolve(__dirname, '..', 'uploads', 'resized'))
+);
 app.use(require('./routes'));
 app.listen(3333);
